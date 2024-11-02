49ee576e15ae34364100fd0e53f1825096ff24e3
vignesh-kg <vigneshkg515@gmail.com>
2024-11-02 11:30:36 +0200

    weird_algorithm

---
+++ weird_algorithm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  long n;
+  cin >> n;
+  cout << n << " ";
+  while(n != 1){
+    if(n%2 == 0) n >>= 1;
+    else n = (n*3)+1;
+    cout << n << " ";  
+  }
+  cout << endl;
+}
